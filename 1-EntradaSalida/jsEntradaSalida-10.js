a5946dbe1864264c37743eafa8e3a9b60ed0a7bc
mezah <60630333+Meza33@users.noreply.github.com>
2020-02-29 21:46:36 -0300

    Update jsEntradaSalida-10.js

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -3,6 +3,18 @@ Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
-{
+{ // Se declaran las variables.
+    var importe;
+    var descuento;
+    var resultado;
+
+    importe=parseInt(document.getElementById("importe").value);
+
+    descuento = (importe*25)/100;
+
+    resultado= importe- descuento;
+
+    document.getElementById("resultado").value=resultado;
+
 	
 }
